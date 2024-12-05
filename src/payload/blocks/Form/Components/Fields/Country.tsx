3d18fee93ba7038797d8c3a13b0527bf8efd9dae
jagadeesh <jagadeeshm778@gmail.com>
2024-12-05 17:43:17 +0530

    chore: country field updated

--- src/payload/blocks/Form/Components/Fields/Country.tsx
+++ src/payload/blocks/Form/Components/Fields/Country.tsx
@@ -1,15 +1,11 @@
 import Error from '../Error'
 import Width from '../Width'
-import Image from 'next/image'
-import { Form } from 'react-bootstrap'
 import {
   FieldErrorsImpl,
   FieldValues,
   UseFormRegister,
   UseFormSetValue,
 } from 'react-hook-form'
-
-import { trpc } from '@/trpc/client'
 
 interface CountryField {
   name: string
@@ -39,35 +35,19 @@ const Country: React.FC<
   required: requiredFromProps,
   errors,
 }) => {
-  const { data: countries, isLoading: isCountriesLoading } =
-    trpc?.form?.getAllCountries.useQuery()
   return (
     <Width width={width as number}>
       <div>
-        <label>{label}</label>
+        <label htmlFor={label ?? ''}>{label}</label>
 
-        <div className='user-form'>
-          <Form.Select
-            aria-placeholder={
-              isCountriesLoading ? 'Loading...' : 'Select country'
-            }
-            className='custom-select'
-            {...register(name, {
-              required: requiredFromProps as boolean,
-              onChange: (event: React.ChangeEvent<HTMLSelectElement>) => {
-                setValue(name, event.target.value)
-              },
-            })}>
-            {countries?.flatMap((country: any, index: number) => (
-              <option
-                className='option'
-                key={index}
-                style={{ display: 'flex', gap: '2px' }}>
-                <Image src={country?.flag} alt='' width={28} height={14} />
-                <p>{country?.name}</p>
-              </option>
-            ))}
-          </Form.Select>
+        <div className='form'>
+          <div className='form-group'>
+            <input
+              type='text'
+              id={label ?? ''}
+              {...register(name, { required: requiredFromProps as boolean })}
+            />
+          </div>
         </div>
         {requiredFromProps && errors[name] && (
           <Error error={errors[name]} label={label!} />
