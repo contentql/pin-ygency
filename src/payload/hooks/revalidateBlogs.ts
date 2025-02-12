4c7c98cc4b54ad9778e13607b212e47d1c6d0608
charanm927 <charanm927@gmail.com>
2025-02-12 22:26:01 +0530

    chore: revalidation hooks after delete

--- src/payload/hooks/revalidateBlogs.ts
+++ src/payload/hooks/revalidateBlogs.ts
@@ -1,10 +1,13 @@
 import { Blog } from '@payload-types'
 import { revalidateTag } from 'next/cache'
-import type { CollectionAfterChangeHook } from 'payload'
+import type {
+  CollectionAfterChangeHook,
+  CollectionAfterDeleteHook,
+} from 'payload'
 
-export const revalidateBlogs: CollectionAfterChangeHook<Blog> = async ({
-  doc,
-}) => {
+export const revalidateBlogsAfterChange: CollectionAfterChangeHook<
+  Blog
+> = async ({ doc }) => {
   // if page is published & their is no dynamic block directly revalidating the page
   if (doc._status === 'published') {
     revalidateTag('list-tags-with-blog-count')
@@ -14,4 +17,17 @@ export const revalidateBlogs: CollectionAfterChangeHook<Blog> = async ({
     revalidateTag(`details-blogs-${doc?.slug}`)
     console.log(`list-blogs at ${new Date().getTime()}`)
   }
+}
+
+export const revalidateBlogsAfterDelete: CollectionAfterDeleteHook<
+  Blog
+> = async ({ doc }) => {
+  // if page is published & their is no dynamic block directly revalidating the page
+
+  revalidateTag('list-tags-with-blog-count')
+  revalidateTag('list-authors-with-blog-count')
+
+  revalidateTag('list-blogs')
+  revalidateTag(`details-blogs-${doc?.slug}`)
+  console.log(`list-blogs at ${new Date().getTime()}`)
 }
--- src/payload/hooks/revalidateTags.ts
+++ src/payload/hooks/revalidateTags.ts
@@ -1,10 +1,13 @@
 import { Tag } from '@payload-types'
 import { revalidateTag } from 'next/cache'
-import type { CollectionAfterChangeHook } from 'payload'
+import type {
+  CollectionAfterChangeHook,
+  CollectionAfterDeleteHook,
+} from 'payload'
 
-export const revalidateTags: CollectionAfterChangeHook<Tag> = async ({
-  doc,
-}) => {
+export const revalidateTagsAfterChange: CollectionAfterChangeHook<
+  Tag
+> = async ({ doc }) => {
   // if page is published & their is no dynamic block directly revalidating the page
   if (doc._status === 'published') {
     revalidateTag('list-tags')
@@ -15,4 +18,18 @@ export const revalidateTags: CollectionAfterChangeHook<Tag> = async ({
 
     console.log(`list-tags at ${new Date().getTime()}`)
   }
+}
+
+export const revalidateTagsAfterDelete: CollectionAfterDeleteHook<
+  Tag
+> = async ({ doc }) => {
+  // if page is published & their is no dynamic block directly revalidating the page
+
+  revalidateTag('list-tags')
+  revalidateTag('list-tags-with-blog-count')
+
+  revalidateTag(`details-tags-${doc.slug}`)
+  revalidateTag(`details-blogs-by-tags-${doc.slug}`)
+
+  console.log(`list-tags at ${new Date().getTime()}`)
 }
